c8236a2eb8aec06a15357f95610e438aa3d61f75
Veikko Jääskeläinen <veikko.jaaskelainen@measurlabs.com>
2025-02-25 16:27:41 +0200

    feat: topnav mobile support

--- app.vue
+++ app.vue
@@ -11,17 +11,7 @@ export default {
 
 <template>
   <div class="page">
-    <header class="header">
-      <div class="header__title">
-        <a class="header__link" href="/">
-          <img class="header__logo"  height="40px" src="/tak_logo.svg" alt="Teekkarien Autokerho" />
-        </a>
-        <a class="header_name" href="/">Teekkarien Autokerho</a>
-        <div class="header__spacer"></div>
-        <a class="header__link" v-for="page in headerPages" :key="page.slug" :href="page.href">{{ page.title }}</a>
-      </div>
-
-    </header>
+    <TopNav />
     <main>
       <NuxtPage />
     </main>
@@ -90,6 +80,7 @@ export default {
   --max-width-2: 1024px;
 
   background-color: #111111;
+  color: #ced2d9;
 }
 
 html {
@@ -110,7 +101,7 @@ body {
 
 .container {
   margin: 72px auto;
-  color: white;
+  color: #ced2d9;
 
   @media (min-width: 575px) {
     max-width: var(--max-width-1);
@@ -120,61 +111,12 @@ body {
     max-width: var(--max-width-2);
   }
 }
-
 </style>
 
 <style scoped>
-
 main {
   margin-top: 45px;
-}
-
-.header {
-  display: flex;
-  padding: 0 var(--space-1);
-  border-bottom: 1px solid #ced2d9;
-
-  z-index: 10;
-  background: var(--white);
-  position: fixed;
-  left: 0;
-  right: 0;
-  top: 0;
-
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.5);
-
-  & .header__title {
-    display: flex;
-    align-items: center;
-    flex: 1;
-    
-    font-size: var(--font-size-3);
-    text-decoration: none;
-    color: var(--black);
-  }
-  & .header_name  {
-    font-size: var(--font-size-5);
-    font-family: var(--font-family-header);
-    font-weight: 800;
-    color: #ed1e24;
-    text-decoration: none;
-  }
-  & .header__spacer {
-    flex: 1;
-  }
-  & .header__link {
-    font-weight: 500;
-    color: #ed1e24;
-    text-decoration: none;
-    &:hover { 
-      color: #f14b50;
-      cursor: pointer;
-    }
-  }
-
-  & .header__logo {
-    height: 56px;
-  }
+  min-height: 1000px;
 }
 
 .footer {
@@ -197,75 +139,18 @@ main {
 }
 .footer_links {
   display: flex;
-  gap: 8px;
+  flex-direction: column;
+  align-items: center;
+  gap: 16px;
+
+  @media (min-width: 575px) {
+    flex-direction: row;
+  }
 }
 
 @media (min-width: 575px) {
-  .container {
-    max-width: var(--max-width-1);
-    padding: 0 var(--space-4);
-  }
-
   main {
     margin-top: unset;
   }
-
-  .header {
-    position: unset;
-    border-bottom: none;
-    
-    padding: var(--space-3) var(--space-5);
-    background-color: #111111;
-    
-
-    & .header__title {
-      gap: 24px;
-      font-size: var(--font-size-5);
-      color: #ed1e24;
-    }
-  }
-
-}
-
-.preview-toggle {
-  backdrop-filter: blur(12px);
-  border-radius: 0.25rem;
-  bottom: 1rem;
-  box-shadow:
-    0 10px 15px -3px rgba(0, 0, 0, 0.1),
-    0 4px 6px -2px rgba(0, 0, 0, 0.05);
-  color: #1f2937;
-  display: block;
-  font-size: 0.75rem;
-  font-weight: 500;
-  line-height: 1rem;
-  padding-bottom: 0.5rem;
-  padding-left: 0.75rem;
-  padding-right: 0.75rem;
-  padding-top: 0.5rem;
-  position: fixed;
-  right: 1rem;
-  text-align: center;
-  text-decoration: none;
-  z-index: 50;
-}
-
-.preview-toggle:hover {
-  background-color: #ef4444;
-  color: #ffffff;
-}
-
-.preview-toggle span:first-child {
-  display: block;
-}
-.preview-toggle:hover span:first-child {
-  display: none;
-}
-
-.preview-toggle span:last-child {
-  display: none;
-}
-.preview-toggle:hover span:last-child {
-  display: block;
 }
 </style>
---
+++ components/topNav.vue
@@ -0,0 +1,191 @@
+<script setup lang="ts">
+import { ref, onMounted, onUnmounted } from "vue";
+import { headerPages } from "#build/imports";
+
+const isMenuOpen = ref(false);
+const menuRef = ref<HTMLElement | null>(null);
+
+const toggleMenu = () => {
+  isMenuOpen.value = !isMenuOpen.value;
+};
+
+const closeMenu = (event: Event) => {
+  if (
+    isMenuOpen.value &&
+    menuRef.value &&
+    !menuRef.value.contains(event.target as Node)
+  ) {
+    isMenuOpen.value = false;
+  }
+};
+
+const handleEscape = (event: KeyboardEvent) => {
+  if (event.key === "Escape") {
+    isMenuOpen.value = false;
+  }
+};
+
+onMounted(() => {
+  document.addEventListener("click", closeMenu);
+  document.addEventListener("keydown", handleEscape);
+});
+
+onUnmounted(() => {
+  document.removeEventListener("click", closeMenu);
+  document.removeEventListener("keydown", handleEscape);
+});
+</script>
+
+<template>
+  <header class="header">
+      <div class="header-content">
+        <a class="header-link" href="/">
+          <img class="header__logo"  height="40px" src="/tak_logo.svg" alt="Teekkarien Autokerho" />
+        </a>
+        <a class="header-name" href="/">Teekkarien Autokerho</a>
+        <div class="header-spacer"></div>
+        <div class="header-links">
+          <a 
+            class="header-link" 
+            v-for="page in headerPages" 
+            :key="page.slug" 
+            :href="page.href"
+            >{{ page.title }}
+          </a>
+        </div>
+        <button class="burger-menu-toggle" @click.stop="toggleMenu">
+        ☰
+        </button>
+      </div>
+      <nav v-if="isMenuOpen" ref="menuRef" class="mobile-menu">
+        <a 
+          class="mobile-menu-item" 
+          v-for="page in headerPages" 
+          :key="page.slug" 
+          :href="page.href"
+          @click="isMenuOpen = false"
+        >
+          {{ page.title }}
+        </a>
+      </nav>
+    </header>
+</template>
+
+<style scoped>
+.header {
+  display: flex;
+  flex-direction: column;
+  padding: 12px  16px;
+  z-index: 10;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  background-color: #111111;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.5);
+
+  @media (min-width: 575px) {
+    padding: 12px 32px;
+  }
+
+  @media (min-width: 1024px) {
+    padding: 18px 32px;
+  }
+}
+
+.header-content {
+  display: flex;
+  align-items: center;
+  flex: 1;
+  gap: 24px;
+  font-size: var(--font-size-2);
+  text-decoration: none;
+  color: #ed1e24;
+
+  @media (min-width: 575px) {
+    font-size: var(--font-size-3);
+  }
+
+  @media (min-width: 1024px) {
+    font-size: var(--font-size-4);
+  }
+}
+
+.header-name {
+  font-size: var(--font-size-5);
+  font-family: var(--font-family-header);
+  font-weight: 800;
+  color: #ed1e24;
+  text-decoration: none;
+  @media (min-width: 1124px) {
+    font-size: var(--font-size-6);
+  }
+}
+
+.header-spacer {
+  flex: 1;
+}
+
+.header-links {
+  display: none;
+  gap: 12px;
+
+  @media (min-width: 900px) {
+    display: flex;
+  }
+  @media (min-width: 1024px) {
+    gap: 24px;
+  }
+}
+
+.header-link {
+  font-weight: 500;
+  color: #ed1e24;
+  text-decoration: none;
+}
+
+.header-link:hover {
+  color: #f14b50;
+  cursor: pointer;
+}
+
+.header-logo {
+  height: 48px;
+
+  @media (min-width: 575px) {
+    height: 56px;
+  }
+}
+
+.burger-menu-toggle {
+  display: block;
+  font-size: 24px;
+  background: none;
+  border: none;
+  color: #ed1e24;
+  cursor: pointer;
+
+  @media (min-width: 900px) {
+    display: none;
+  }
+}
+
+.mobile-menu {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  gap: 8px;
+}
+
+.mobile-menu-item {
+  color: #ed1e24;
+  text-decoration: none;
+  font-size: var(--font-size-3);
+  padding: 8px 10px;
+}
+
+.mobile-menu-item:hover {
+  color: #f14b50;
+  cursor: pointer;
+}
+</style>
